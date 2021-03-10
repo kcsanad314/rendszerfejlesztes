23f9574397889a9a6d0acc89add816564e5d3ce1
kcsanad314 <61548482+kcsanad314@users.noreply.github.com>
2021-03-10 20:36:24 +0100

    database creation

---
+++ Netpincer_App_Beadando/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Netpincer_App_Beadando.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Netpincer_App_Beadando.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        //public User user { get; set; }
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // POST: UserController/Create
+        [HttpPost]
+        public ActionResult Create([FromBody] User user)
+        {
+            _db.Users.Add(user);
+            _db.SaveChanges();
+            return Ok();
+        }
+        [HttpGet]
+        public ActionResult test()
+        {
+            return Ok("asd");
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Netpincer_App_Beadando/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Netpincer_App_Beadando.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using System.Data.Entity;
+
+
+
+namespace Netpincer_App_Beadando.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    public DbSet <User> Users { get; set; }
+
+    }
+}
