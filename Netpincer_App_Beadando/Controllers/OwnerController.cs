5eb14d41c65d1cfeed8aefeea51d59591c412392
kcsanad314 <61548482+kcsanad314@users.noreply.github.com>
2021-03-26 22:24:27 +0100

    Restaurant registration, adding foodcategory/foods

---
+++ Netpincer_App_Beadando/Controllers/OwnerController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Netpincer_App_Beadando.Models;
+using Netpincer_App_Beadando.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Netpincer_App_Beadando.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OwnerController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public OwnerController(ApplicationDbContext db)
+        {
+            _db = db;
+            //_mapper = mapper;
+        }
+        [HttpPost]
+        public IActionResult RegisterRestaurant([FromBody] Restaurant restaurant)
+        {
+            //TODO: check if User has Owner or Admin UserType
+            //TODO: check if restaurant already exists
+            _db.Restaurants.Add(restaurant);
+            _db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddFoodCategory(FoodCategory foodCategory)
+        {
+            
+            FoodCategory fc = new FoodCategory() {  
+                Name = foodCategory.Name,
+                RestaurantId = foodCategory.RestaurantId
+            };
+            fc.Foods = new List<Food>();
+            foodCategory.Foods.ForEach(food =>
+            {
+                Food _food = new Food()
+                {
+                    Name = food.Name,
+                    Price = food.Price,
+                    Allergenes = food.Allergenes
+                };
+                fc.Foods.Add(_food);
+            });
+            Restaurant rest = _db.Restaurants.Find(fc.RestaurantId);
+            rest.FoodCategories = new List<FoodCategory>();
+            rest.FoodCategories.Add(fc);
+            _db.Add(fc);
+            _db.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Netpincer_App_Beadando/Models/Entity/FoodCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Netpincer_App_Beadando.Models.Entity
+{
+    public class FoodCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Kötelező a(z) {0} mezőt kitölteni!")]
+        [MaxLength(255, ErrorMessage = "A(z) {0} maximum {1} karakter lehet.")]
+        [Display(Name = "Kategória neve", AutoGenerateFilter = false, AutoGenerateField = false, Order = 0)]
+        public string Name { get; set; }
+        public Restaurant Restaurant { get; set; }
+        public int RestaurantId { get; set; }
+        public List<Food> Foods { get; set; }
+    }
+}
